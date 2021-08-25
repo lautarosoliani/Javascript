15148b665bc81b6d82f836bec1e3a60dadc6503d
Lautaro <lautarosoliani@hotmail.com>
2021-08-25 02:29:39 -0300

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const num1 = 20
+const num2 = 20
+
+if (num1 > num2) {
+    console.log(`${num1} es mayor que ${num2}`)
+} else if (num1 < num2) {
+    console.log(`${num1} es menor que ${num2}`)
+} else {
+    console.log("Los numeros son iguales")
+}
+
+// --> ESTRUCTURA DE UN IF
+// if (aca va la condicion) {aca va la consecuencia si la condicion es true}

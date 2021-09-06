61bf571d34b09c6a03ec5d5f34a9c4e3979ce218
Lautaro <lautarosoliani@hotmail.com>
2021-09-06 09:40:58 -0300

    exercise event.target.value

--- Meeting 30 - Formularios/script.js
+++ Meeting 30 - Formularios/script.js
@@ -2,3 +2,40 @@
 ////////////////////////////////////////////////////////////////////////
 ///////////////////////////MEETING 30///////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
+
+// TOOLBOX
+// event.target.value
+
+//
+//
+//
+
+// import React, { useState } from "react";
+// import "./styles.css";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//       <Input />
+//     </div>
+//   );
+// }
+
+// function Input() {
+//   const [inputState, setInputState] = useState(null);
+
+//   const handleInput = (event) => {
+//     setInputState(event.target.value);
+
+//     console.log(`el valor del input es:`);
+//     console.log(event.target.value);
+//   };
+
+//   return (
+//     <>
+//       <input type="text" onChange={handleInput}></input>
+//     </>
+//   );
+// }

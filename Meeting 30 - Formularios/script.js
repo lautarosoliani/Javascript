e913fc17eafd33459e2ab13a621d1952fba60861
Lautaro <lautarosoliani@hotmail.com>
2021-09-06 10:08:29 -0300

    date exercise finished

--- Meeting 30 - Formularios/script.js
+++ Meeting 30 - Formularios/script.js
@@ -51,3 +51,38 @@
 //
 //
 //
+// import React, { useState } from "react";
+// import "./styles.css";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//       <Input />
+//     </div>
+//   );
+// }
+
+// function Input() {
+//   const [inputState, setInputState] = useState(``);
+//   // const [tiempoUNIX, setTiempoUNIX] = useState(0)
+
+//   const handleInput = (event) => {
+//     const tiempoUNIX = new Date(event.target.value).getTime();
+//     const dateYear = dateString.getFullYear()
+
+//     console.log(`el valor del input es:`);
+//     console.log(event.target.value);
+//     console.log(tiempoUNIX);
+//     setInputState(event.target.value);
+//   };
+
+//   return (
+//     <>
+//       <input type="date" onChange={handleInput} value={inputState}></input>
+//     </>
+//   );
+// }
+
+// // https://www.youtube.com/watch?v=If1lfe-Fhc8&t=2s

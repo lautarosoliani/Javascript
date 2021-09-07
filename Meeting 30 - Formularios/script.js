c23abcafa8939e5dedadcb643ddb165a3d43e394
Lautaro <lautarosoliani@hotmail.com>
2021-09-07 08:10:33 -0300

    conditional render with ternary operator

--- Meeting 30 - Formularios/script.js
+++ Meeting 30 - Formularios/script.js
@@ -199,3 +199,68 @@
 //     </div>
 //   );
 // }
+
+// PRODUCCION GRAFICA FUNCIONAL
+// https://codesandbox.io/s/produccion-grafica-funcional-clie2?file=/src/logos/001-amex.svg
+
+// import React from "react";
+// import "./styles.css";
+// import amex from "./logos/001-amex.svg";
+// import visa from "./logos/002-visa.svg";
+// import mastercard from "./logos/003-mastercard.svg";
+
+// export default function App() {
+//   const mostrarTarjeta = (dimension, source, alt) => {
+//     return <img height={dimension} src={source} alt={alt} />;
+//   };
+//   return (
+//     <div className="container">
+//       {mostrarTarjeta(visa, "visa", "50px")}
+//       {mostrarTarjeta(amex, "amex", "50px")}
+//       {mostrarTarjeta(mastercard, "mastercard", "50px")}
+//     </div>
+//   );
+// }
+
+// RENDERIZADO CONDICIONAL
+
+import React, { useState } from "react"
+
+// importar estilos
+import "./styles.css"
+
+// importar imágenes
+import check from "./logos/001-cheque.svg"
+import cancel from "./logos/002-cancelar.svg"
+
+export default function App() {
+    let [nombre, actualizarNombre] = useState("")
+
+    const manejarCambioNombre = (evento) => {
+        actualizarNombre(evento.target.value)
+    }
+
+    return (
+        <div className="container">
+            <div>
+                <div className="input-container">
+                    <input
+                        className="input-nombre"
+                        value={nombre}
+                        onChange={manejarCambioNombre}
+                        type="text"
+                    />
+                    {nombre.length > 6 ? (
+                        <img height="30px" src={check} alt="" />
+                    ) : (
+                        <img height="30px" src={cancel} alt="" />
+                    )}
+                </div>
+                <div className="msj">
+                    El nombre no de estar vacío y debe tener más de 6
+                    caracteres.
+                </div>
+            </div>
+        </div>
+    )
+}
